b58af17f522c94314c531f23b2502c1f63e3e5af
Stepan Kuznetsov <mindvr@gmail.com>
2020-04-22 14:44:46 +0300

    fixes #3 added transpose option
    parse switced to using extracted parser/writer modules

--- parse.py
+++ parse.py
@@ -1,6 +1,8 @@
 import sys
 import re
 import argparse 
+from parselogs import parselogs, Measure
+from writedata import writedata, Value
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--transpose', action='store_true')
@@ -8,76 +10,11 @@ parser.add_argument('--stdout', action='store_true')
 parser.add_argument('--output', action='store', default='data/output.csv')
 parser.add_argument('input_file', nargs='?', default='data/source.txt')
 
-dateFormat = re.compile('\d\d\.\d\d\.\d\d')
-regionFormat = re.compile('🔸.*-')
-
-
-def readfile(name):
-    file = open(name, 'r')
-    lines = file.readlines()
-    file.close()
-    return lines
-
-
-def clean(lines):
-    return [line for line in lines if regionFormat.search(line) or dateFormat.search(line)]
-
-
-def parse_lines(lines):
-    parse_result = {}
-    current_date = '00.00'
-    for line in lines:
-        if dateFormat.search(line):
-            date_tokens = dateFormat.findall(line)[0].split('.')
-            current_date = date_tokens[2]+'-'+date_tokens[1]+'-'+date_tokens[0]
-            parse_result[current_date] = {}
-        else:
-            tokens = line.split()
-            region = regionFormat.findall(line)[0][1:-1].strip()
-            parse_result[current_date][region] = tokens[-1]
-    return parse_result
-
-
-def get_dates(parse_result):
-    dates = list(parse_result.keys())
-    dates.sort()
-    return dates
-
-def get_header(dates):
-    return '\t' + '\t'.join(dates)
-
-def group_by_region(parse_result):
-    transformation = {}
-    for date in parse_result:
-        for city in parse_result[date]:
-            if(not city in transformation):
-                transformation[city] = {}
-            transformation[city][date] = parse_result[date][city]
-    return transformation
-
-def write_lines(name, lines):
-    file = open(name, 'w')
-    file.write('\n'.join(lines))
-    file.close()
-
-
 if __name__ == '__main__':
     args = parser.parse_args()
-    lines = clean(readfile(args.input_file))
-    parse_result = parse_lines(lines)
-    dates = get_dates(parse_result)
-    result = []
-    result.append(get_header(dates))
-    byRegions = group_by_region(parse_result)
-    regionsSorted = list(byRegions.keys())
-    regionsSorted.sort()
-    for region in regionsSorted:
-        row = region
-        for date in dates:
-            row += '\t'
-            if date in byRegions[region]:
-                row += byRegions[region][date]
-            else:
-                row += '0'
-        result.append(row)
-    write_lines("data/output.csv", result)
+    logs = parselogs(args.input_file)
+    if args.transpose:
+        values = [Value(m.city, m.date, m.count) for m in logs]
+    else:
+        values = [Value(m.date, m.city, m.count) for m in logs]
+    writedata(values, args.output)
