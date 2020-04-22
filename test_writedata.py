0283d49c4a731ba3c64d30b2bba3e5ae9f81fdf3
Stepan Kuznetsov <mindvr@gmail.com>
2020-04-22 14:31:39 +0300

    extracted and generified  data writer

---
+++ test_writedata.py
@@ -0,0 +1,18 @@
+import unittest
+from writedata import *
+
+test_data = [Value('2020-04-15', 'Москва', 1355), Value('2020-04-15', 'Волгоградская область', 6),
+             Value('2020-04-13', 'Москва', 1489), Value('2020-04-13', 'Московская область', 460)]
+
+
+class WritedataTest(unittest.TestCase):
+    def test_get_rows(self):
+        expected = [
+            ['', '2020-04-13', '2020-04-15'],
+            ['Волгоградская область', 0, 6],
+            ['Москва', 1489, 1355],
+            ['Московская область', 460, 0]
+        ]
+        got = get_rows(test_data)
+        print(got)
+        self.assertEqual(got, expected)
